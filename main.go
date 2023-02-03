26daba42386a1883ad13be16afc3782c93de610d
Chien Anubis <iocutien2112@gmail.com>
2023-02-03 14:36:52 +0700

    feat: create a grpc server
    Signed-off-by: Chien Anubis <iocutien2112@gmail.com>

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import (
 	"database/sql"
 	_ "github.com/lib/pq"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log"
+	"net"
 	"simple_bank/api"
 	db "simple_bank/db/sqlc"
+	"simple_bank/gapi"
+	"simple_bank/pb"
 	"simple_bank/utils"
 )
 
@@ -22,6 +27,38 @@ func main() {
 	}
 
 	store := db.NewSQLStore(conn)
+	runGinServer(config, store)
+	//runGrpcServer(config, store)
+}
+
+func runGrpcServer(config utils.Config, store db.Store) {
+	server, err := gapi.NewServer(config, store)
+
+	if err != nil {
+		log.Fatal("cannot create server", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	pb.RegisterSimpleBankServer(grpcServer, server)
+
+	// this command look very simple, but it actually is pretty powerful,
+	// because it allows the gRPC client to easily explore
+	// what RPCs are available on the server, and how to call them, (like enable feature GraphQL Playground)
+	reflection.Register(grpcServer)
+
+	listener, err := net.Listen("tcp", config.GRPCServerAddress)
+	if err != nil {
+		log.Fatal("cannot create listener", err)
+	}
+
+	log.Printf("start gRPC server at %s", listener.Addr().String())
+	err = grpcServer.Serve(listener)
+	if err != nil {
+		log.Fatal("cannot start gRPC server", err)
+	}
+}
+
+func runGinServer(config utils.Config, store db.Store) {
 	server, err := api.NewServer(config, store)
 
 	if err != nil {
