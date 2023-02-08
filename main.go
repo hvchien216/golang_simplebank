dff7486aee97b549900e87e9db6e6c84f4976416
Chien Anubis <iocutien2112@gmail.com>
2023-02-08 16:07:24 +0700

    feat: run migrate in app
    Signed-off-by: Chien Anubis <iocutien2112@gmail.com>

--- main.go
+++ main.go
@@ -3,6 +3,9 @@ package main
 import (
 	"context"
 	"database/sql"
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	_ "github.com/lib/pq"
 	"github.com/rakyll/statik/fs"
@@ -32,10 +35,26 @@ func main() {
 		log.Fatal("cannot connect to db:", err)
 	}
 
+	//run db migration
+	runDBMigration(config.MigrationURL, config.DBSource)
+
 	store := db.NewSQLStore(conn)
 	//runGinServer(config, store)
 	go runGatewayServer(config, store)
 	runGrpcServer(config, store)
+}
+
+func runDBMigration(migrationURL string, dbSource string) {
+	migration, err := migrate.New(migrationURL, dbSource)
+	if err != nil {
+		log.Fatal("cannot create new migrate instance", err)
+	}
+
+	if err = migration.Up(); err != nil && err != migrate.ErrNoChange {
+		log.Fatal("failed to run migrate up")
+	}
+
+	log.Println("db migrated successfully")
 }
 
 func runGrpcServer(config utils.Config, store db.Store) {
